8e0757113fa83dd95d417ab528d9fceac2034964
rasec1106 <elch.herrera@gmail.com>
2022-12-04 05:43:19 -0500

    String connection

--- src/main/java/utils/MySqlDBConnection.java
+++ src/main/java/utils/MySqlDBConnection.java
@@ -8,7 +8,7 @@ public class MySqlDBConnection {
 		Connection cn = null;
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			cn = DriverManager.getConnection("jdbc:mysql://localhost/proyecto_lp2?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "root","cleu1106Q!");
+			cn = DriverManager.getConnection("jdbc:mysql://18.207.63.198:3306/proyecto_LP2?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", "cibertec","cibertec123");
 		}catch(Exception e) {
 			e.printStackTrace();
 		}
